f6d9c0cde81c348a086506594ec39a8d6d2dcc51
Zane <76492358+ZaneZJ@users.noreply.github.com>
2022-02-26 17:21:49 +0200

    Updated 22.02.2022 class

---
+++ features/support/pages.js
@@ -0,0 +1,9 @@
+import { BasePage } from "../pageobjects/basePage";
+
+class Pages {
+    constructor() {
+        thisbasePage = new BasePage();
+    }
+}
+
+export const pages = new Pages();
---
+++ features/testobjects/navigationTest.js
@@ -0,0 +1,6 @@
+export class NavigationTest {
+    async openOracleProfilePage() {
+        await browser.url('/')
+        // or in the place of / can just place the URL of oracle profile
+    }
+}
